a3bea895792656447c8e2ae28db25a2a6e6ab6fe
github username <github email address>
2021-08-30 20:50:44 -0500

    commit tree bbb57a0e7b0cfc15e74aac0a4d4981af1dfe4a9a

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,16 +1,22 @@
 class Backer
 
-    attr_accessor :name
-
-    @@all = []
+    attr_reader :name
 
     def initialize(name)
         @name = name
-        @@all << self
     end
 
-    def self.all
-        @@all
+    def back_project(project)
+        ProjectBacker.new(project,self)
+    end
+
+    def backed_projects
+        project_backers = ProjectBacker.all.select do |pb|
+            pb.backer == self
+        end
+        project_backers.map do |pb|
+            pb.project
+        end
     end
 
 
